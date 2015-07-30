24f9d673397fe2ed19df65837997523ddde39578
Demetri Ganoff <dganoff@gmail.com>
2015-07-30 17:32:02 -0400

    scroll to top of page on state change

--- src/app/app.run.js
+++ src/app/app.run.js
@@ -6,13 +6,23 @@
     .run(run);
 
   /* @ngInject */
-  function run(GithubService) {
+  function run(GithubService, $rootScope) {
     activate();
 
     //////////
 
     function activate () {
       GithubService.setHeaders();
+      scrollToTop();
+    }
+
+    function scrollToTop () {
+      /**
+       * Scroll to the top of the viewport upon state change
+       */
+      $rootScope.$on("$stateChangeSuccess", function() {
+        document.body.scrollTop = document.documentElement.scrollTop = 0;
+      });
     }
   }
 })();
