e3c35784886e0a1e448c43ad7220e6c353e96bd5
Demetri Ganoff <dganoff@gmail.com>
2015-07-29 21:26:24 -0400

    heading tweak

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ This will get you the necessary node and bower packages to do everything
 within this project. It's recommended that you install the grunt-cli globally
 (`$ npm install -g grunt-cli`) so you can use `grunt` for the tasks.
 
-## Development
+## Local Development & Build
 
 Run `grunt serve` to build the app and start the local server at `http://localhost:9000`.
 
--- src/app/app.config.js
+++ src/app/app.config.js
@@ -9,6 +9,17 @@
   function configure($stateProvider, $urlRouterProvider) {
     // For any unmatched url, redirect here:
     $urlRouterProvider
-      .otherwise("/");
+      .otherwise("/org");
+
+    // Application States/Routes:
+    $stateProvider
+      // --------------------------------------------------
+      // Home:
+      // --------------------------------------------------
+      .state("org", {
+        url: "/org",
+        templateUrl: "app/organization/organization.html",
+        controller: "OrganizationCtrl as vm",
+      });
   }
 })();
